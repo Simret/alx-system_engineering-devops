b293cba2f88e9b9d510ec065e6e68c9d5f2f3f56
simret <simruineb@gmail.com>
2022-11-15 00:58:01 +0300

    Get data

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' Get some data from API '''
+
+
+from requests import get
+import requests
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+    ''' To get data from API'''
+    t = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                     .format(argv[1]))
+    t = t.json()
+    us = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                      .format(argv[1]))
+    us = us.json()
+    u = us["username"]
+    f = open('./{}.csv'.format(argv[1]), 'w')
+    wr = csv.writer(f, quoting=csv.QUOTE_ALL)
+    for i in t:
+        if (((i["userId"]) == int(argv[1]))):
+            wr.writerow([str(
+                i["userId"]), u, i["completed"], i["title"]])
