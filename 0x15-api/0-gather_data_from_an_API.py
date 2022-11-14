693fd2ddc5ce73de41ac19ae4da9bcdc0e0485c5
simret <simruineb@gmail.com>
2022-11-15 00:49:16 +0300

    Get employee data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,16 +6,22 @@ from sys import argv
 
 if __name__ == "__main__":
     """ Get employees todo list """
-    ID = int(argv[1])
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(ID)).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(ID)).json()
-    tasks = []
-    for task in todo:
-        if task.get('completed') is True:
-            tasks.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):".
-          format(user.get('name'), len(tasks), len(todo)))
-    for task in tasks:
-        print("\t {}".format(task))
+    list = []
+    a = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                     .format(argv[1]))
+    a = a.json()
+    b = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                     .format(argv[1]))
+    b = b.json()
+    x = 0
+    y = 0
+    for i in a:
+        if (((i["userId"]) == int(argv[1]))):
+            x += 1
+            if (i["completed"]):
+                y += 1
+                list = list + [(i["title"])]
+    print("Employee {} is done with tasks({}/{}):".format(
+        b["name"], y, x))
+    for i in list:
+        print("\t", i)
