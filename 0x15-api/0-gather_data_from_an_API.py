79a366e740a0b3725998ac5eee1e0e96bfbf534d
simret <simruineb@gmail.com>
2022-11-16 14:50:37 +0300

    Fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-import requests
-from sys import argv
-""" Accessing a url with employee ID to return information about employee"""
+'''Retrieves data using a RESTful API'''
 
 
-if __name__ == "__main__":
-    """ Get employees data, using url with employee ID & return information """
-    list = []
-    a = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                     .format(argv[1]))
-    a = a.json()
-    b = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                     .format(argv[1]))
-    b = b.json()
-    x = 0
-    y = 0
-    for i in a:
-        if (((i["userId"]) == int(argv[1]))):
-            x += 1
-            if (i["completed"]):
-                y += 1
-                list = list + [(i["title"])]
-    print("Employee {} is done with tasks({}/{}):".format(
-        b["name"], y, x))
-    for i in list:
-        print("\t", i)
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    id = sys.argv[1]
+    site = 'https://jsonplaceholder.typicode.com'
+    user_url = site + '/users/' + id
+    task_done_url = site + '/todos?userId=' + id + '&completed=true'
+    task_not_done_url = site + '/todos?userId=' + id + '&completed=false'
+
+    user = requests.get(user_url)
+    todos = requests.get(task_done_url)
+    todos_undone = requests.get(task_not_done_url)
+    task_done = len(todos.json())
+    task_not_done = len(todos_undone.json())
+    task_tot = task_done + task_not_done
+    name = user.json().get('name')
+
+    print('Employee {} is done with tasks({}/{}):'.format(name,
+                                                          task_done,
+                                                          task_tot))
+    for i in todos.json():
+        print('\t ' + i.get('title'))
