a44bc866deb44310318bc9f24b692bb9edfb47d2
simret <simruineb@gmail.com>
2022-12-01 22:35:19 +0300

    Titles of the top ten hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
+"""API"""
 import requests
-"""
-functions that queries the reddit api
-"""
 
 
 def top_ten(subreddit):
     """
-    function definition that prints the titles of first 10 hot posts listed
-    for a given subreddit
+     a function that queries the Reddit API and prints the titles
+     of the first 10 hot posts listed for a given subreddit
+     subreddit : name of fild
     """
-    url = "https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit)
-    header = {'User-Agent': 'CustomClient/1.0'}
-    req = requests.get(url, headers=header, allow_redirects=False)
-    if req.status_code != 200:
-        print(None)
-        return
-    req = req.json()
-    if "data" in req:
-        for post in req.get("data").get("children"):
-            print(post.get("data").get("title"))
+    data = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                        .format(subreddit),
+                        headers={"User-Agent": "My-User-Agent"},
+                        allow_redirects=False)
+    if data.status_code >= 300:
+        print('None')
+
     else:
-        print(None)
+        data_dict = data.json()
+        for child in data_dict.get("data").get("children"):
+            print(child.get("data").get("title"))
