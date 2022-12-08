9f035af2bfbd77fb8975d2060b3ae1ab00c860fb
simret <simruineb@gmail.com>
2022-12-08 11:56:31 +0300

    OS configuration change

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# Changing the OS configuration
+exec { 'hard':
+  command => 'sed -i "/holberton hard/s/5/20000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'soft':
+  command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
